00a9dfeef20d836f68bc16c7f69c30275fc5b7c4
Manuel Castañón-Puga <puga@uabc.edu.mx>
2022-11-07 10:30:26 -0800

    CicrcleCI config

--- src/test/java/org/example/MiClaseTest.java
+++ src/test/java/org/example/MiClaseTest.java
@@ -1,4 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MiClaseTest {
-  
+
+    @Test
+    void getNombre() {
+        MiClase miClase = new MiClase("Pepe");
+        assertEquals("Pepe", miClase.getNombre());
+    }
+
+    @Test
+    void setNombre() {
+        MiClase miClase = new MiClase("Pepe");
+        miClase.setNombre("Juan");
+        assertEquals("Juan", miClase.getNombre());
+    }
 }
